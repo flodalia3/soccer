5d22fc6a5490b6059aaa26a089f5aa06812a1bdf
flodalia3 <florinda.dalia3@gmail.com>
2022-07-27 17:38:17 +0200

    add @CrossOrigin in RefereeController

--- src/main/java/it/accenture/soccer/controllers/RefereeController.java
+++ src/main/java/it/accenture/soccer/controllers/RefereeController.java
@@ -6,6 +6,7 @@ import it.accenture.soccer.model.Referee;
 import it.accenture.soccer.services.implementations.RefereeCrudService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import java.util.stream.StreamSupport;
 
 @RestController
 @RequestMapping("referee")
+@CrossOrigin
 public class RefereeController {
     private RefereeCrudService crudService;
 
